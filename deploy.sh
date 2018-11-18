1e788dda100db052ef1fc46b48080769a1f386b4
lianjia521 <33539160+lianjia521@users.noreply.github.com>
2018-11-19 00:03:58 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -33,7 +33,7 @@ rm -f $TOMCAT_APP_PATH/webapps/order.war
 
 # 复制新的工程
 cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
-
+echo $TOMCAT_APP_PATH
 cd $TOMCAT_APP_PATH/webapps/
 mv order.war ROOT.war
 
